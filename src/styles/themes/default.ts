765d86c0893232b54906112af306cad780912d8e
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2024-06-20 22:15:30 -0300

    feat: add font, shadow and backdrop variants to default theme

--- src/styles/themes/default.ts
+++ src/styles/themes/default.ts
@@ -1,3 +1,5 @@
+const fontFamily = '"Montserrat", sans-serif'
+
 export const defaultTheme = {
   'blue-700': '#1D314B',
   'blue-400': '#506BF1',
@@ -7,5 +9,20 @@ export const defaultTheme = {
 
   'green-400': '#0EAF94',
 
-  'font-montserrat': '"Montserrat", sans-serif',
+  'title-lg': `700 2.25rem ${fontFamily}`,
+  'title-sm': `700 1.25rem ${fontFamily}`,
+
+  text: `600 1rem/1.625rem ${fontFamily}`,
+  'text-sm': `600 0.875rem/1.25rem ${fontFamily}`,
+  'text-xs': `600 0.75rem/1rem ${fontFamily}`,
+
+  input: `700 1rem ${fontFamily}`,
+  'input-sm': `700 0.875rem ${fontFamily}`,
+  'input-lg': `700 1.625rem ${fontFamily}`,
+  'input-xl': `700 2.25rem ${fontFamily}`,
+
+  shadow: '0 4px 4px 0 rgb(0 0 0 / 25%)',
+  'shadow-inner': 'inset 0 4px 4px 0 rgb(0 0 0 / 25%)',
+
+  'backdrop-blur': 'blur(5px);',
 } as const
