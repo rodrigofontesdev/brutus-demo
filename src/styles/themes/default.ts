8f2bfcc1af628dd99721955d4805810a70189034
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2024-06-21 11:15:05 -0300

    refactor: decrease input font-weight in default theme

--- src/styles/themes/default.ts
+++ src/styles/themes/default.ts
@@ -16,10 +16,10 @@ export const defaultTheme = {
   'text-sm': `600 0.875rem/1.25rem ${fontFamily}`,
   'text-xs': `600 0.75rem/1rem ${fontFamily}`,
 
-  input: `700 1rem ${fontFamily}`,
-  'input-sm': `700 0.875rem ${fontFamily}`,
-  'input-lg': `700 1.625rem ${fontFamily}`,
-  'input-xl': `700 2.25rem ${fontFamily}`,
+  input: `600 1rem ${fontFamily}`,
+  'input-sm': `600 0.875rem ${fontFamily}`,
+  'input-lg': `600 1.625rem ${fontFamily}`,
+  'input-xl': `600 2.25rem ${fontFamily}`,
 
   shadow: '0 4px 4px 0 rgb(0 0 0 / 25%)',
   'shadow-inner': 'inset 0 4px 4px 0 rgb(0 0 0 / 25%)',
