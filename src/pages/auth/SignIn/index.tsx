de82ac724bfe4d1317dbaacc3cb527873416f569
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2024-06-21 21:45:42 -0300

    feat(page): add sign in page content

--- src/pages/auth/SignIn/index.tsx
+++ src/pages/auth/SignIn/index.tsx
@@ -1,3 +1,37 @@
+import { faAngleRight } from '@fortawesome/free-solid-svg-icons'
+import { Link } from 'react-router-dom'
+import { Button } from '../../../components/atoms/Button'
+import { Divider } from '../../../components/atoms/Divider'
+import { InputGroup } from '../../../components/molecules/InputGroup'
+import { Container, CreateAccount, SignInForm } from './styles'
+
 export function SignIn() {
-  return <h1>Sign-in</h1>
+  return (
+    <Container>
+      <header>
+        <h1>Acessar Conta</h1>
+        <p>Acesse sua conta utilizando o CNPJ da empresa.</p>
+      </header>
+
+      <SignInForm>
+        <InputGroup.Root>
+          <InputGroup.Label inputId="businessCnpj" text="CNPJ" />
+          <InputGroup.MaskControl
+            mask="00.000.000/0000-00"
+            id="businessCnpj"
+            placeholder="Número do CNPJ"
+          />
+          {/* <InputGroup.Error message="O campo CNPJ é obrigatório." /> */}
+        </InputGroup.Root>
+
+        <Button type="submit" icon={faAngleRight} variant="success" />
+      </SignInForm>
+
+      <Divider />
+
+      <CreateAccount>
+        <Link to="/sign-up">Não tem uma conta? Clique aqui.</Link>
+      </CreateAccount>
+    </Container>
+  )
 }
