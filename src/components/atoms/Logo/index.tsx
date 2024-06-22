0be6a3f9c54e5509d03e4c55c5fbd3733ee006fe
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2024-06-21 21:37:06 -0300

    refactor(atom): set width and height in logo image

--- src/components/atoms/Logo/index.tsx
+++ src/components/atoms/Logo/index.tsx
@@ -1,5 +1,5 @@
 import logoImg from '../../../assets/brutus-logo.svg'
 
 export function Logo() {
-  return <img src={logoImg} alt="Brutus" />
+  return <img src={logoImg} alt="Brutus" width={207} height={50} loading="lazy" />
 }
