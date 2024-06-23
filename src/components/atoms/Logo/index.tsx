9f4b2a067819dc1a5c4b8d60a225c48cc3df104d
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2024-06-22 21:43:19 -0300

    feat(atom): provide link to logo

--- src/components/atoms/Logo/index.tsx
+++ src/components/atoms/Logo/index.tsx
@@ -1,5 +1,11 @@
+import { Link } from 'react-router-dom'
+
 import logoImg from '../../../assets/brutus-logo.svg'
 
 export function Logo() {
-  return <img src={logoImg} alt="Brutus" width={207} height={50} loading="lazy" />
+  return (
+    <Link to="/">
+      <img src={logoImg} alt="Brutus" width={207} height={50} />
+    </Link>
+  )
 }
