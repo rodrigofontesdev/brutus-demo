5ee5517b0a8cefa23a8cce6ba3f228b7b2e2041a
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2024-06-22 21:48:57 -0300

    refactor(atom): remove mandatory props from input mask component

--- src/components/atoms/InputMask/index.tsx
+++ src/components/atoms/InputMask/index.tsx
@@ -1,9 +1,5 @@
-import { IMaskInputProps } from 'react-imask'
 import { InputMaskStyle } from './styles'
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export type InputMaskProps = { mask: any; id: string } & IMaskInputProps<HTMLInputElement>
-
-export function InputMask({ mask, id, ...rest }: InputMaskProps) {
-  return <InputMaskStyle mask={mask} id={id} name={id} autoComplete="off" {...rest} />
+export function InputMask({ ...rest }) {
+  return <InputMaskStyle {...rest} />
 }
