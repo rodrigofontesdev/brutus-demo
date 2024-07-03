3b38bd510ba44d178808e217b274111f8cac14e6
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2024-07-03 16:42:54 -0300

    feat(atom): add size variant to the label component

--- src/components/atoms/Label/index.tsx
+++ src/components/atoms/Label/index.tsx
@@ -4,11 +4,12 @@ import { LabelStyle } from './styles'
 export type LabelProps = {
   text: string
   inputId: string
+  size?: 'normal' | 'large'
 } & LabelHTMLAttributes<HTMLLabelElement>
 
-export function Label({ text, inputId, ...rest }: LabelProps) {
+export function Label({ text, inputId, size, ...rest }: LabelProps) {
   return (
-    <LabelStyle htmlFor={inputId} {...rest}>
+    <LabelStyle htmlFor={inputId} {...rest} $size={size}>
       {text}
     </LabelStyle>
   )
--- src/components/atoms/Label/styles.ts
+++ src/components/atoms/Label/styles.ts
@@ -1,10 +1,12 @@
 import styled from 'styled-components'
 
-export const LabelStyle = styled.label`
-  font: ${(props) => props.theme['input']};
+export const LabelStyle = styled.label<{ $size?: 'normal' | 'large' }>`
   color: ${(props) => props.theme['blue-50']};
+  font: ${(props) =>
+    !props.$size || props.$size === 'normal' ? props.theme['input-sm'] : props.theme['input']};
 
   @media (max-width: 640px) {
-    font: ${(props) => props.theme['input-sm']};
+    font: ${(props) =>
+      !props.$size || props.$size === 'normal' ? props.theme['input-xs'] : props.theme['input-sm']};
   }
 `
