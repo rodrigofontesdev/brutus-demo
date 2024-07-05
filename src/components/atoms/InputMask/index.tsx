c2eb89bd7ae70457b2ff4dd88494d2f738f8d3c9
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2024-07-04 22:16:58 -0300

    feat(atom): add size variant to the input mask component
    Remove size styles from mobile breakpoint.

--- src/components/atoms/InputMask/index.tsx
+++ src/components/atoms/InputMask/index.tsx
@@ -1,13 +1,30 @@
+import { forwardRef } from 'react'
 import { IMaskInputProps } from 'react-imask'
 import { Container, InputMaskStyle, Prefix } from './styles'
 
-type InputMaskProps = { prefix?: string } & IMaskInputProps<HTMLInputElement>
-
-export function InputMask({ prefix, ...rest }: InputMaskProps) {
-  return (
-    <Container>
-      {prefix && <Prefix>{prefix}</Prefix>}
-      <InputMaskStyle {...rest} />
-    </Container>
-  )
+type InputMaskProps = IMaskInputProps<HTMLInputElement> & {
+  mask: any
+  id: string
+  prefix?: string
+  variant?: 'normal' | 'large'
 }
+
+export const InputMask = forwardRef<HTMLInputElement, InputMaskProps>(
+  ({ mask, id, prefix, variant, ...rest }: InputMaskProps, ref) => {
+    return (
+      <Container>
+        {prefix && <Prefix $variant={variant}>{prefix}</Prefix>}
+        <InputMaskStyle
+          mask={mask}
+          prefix={prefix}
+          $variant={variant}
+          id={id}
+          name={id}
+          autoComplete="off"
+          inputRef={ref}
+          {...rest}
+        />
+      </Container>
+    )
+  }
+)
