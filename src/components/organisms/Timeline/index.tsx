85eecfbe170551a430d05c011fa65a47ad92e4ef
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2024-07-10 19:25:51 -0300

    refactor(organism): swap <h3> for <h2> heading in timeline component

--- src/components/organisms/Timeline/index.tsx
+++ src/components/organisms/Timeline/index.tsx
@@ -5,37 +5,37 @@ export function Timeline() {
   return (
     <TimelineStyle>
       <TitleWithSeparator orientation="vertical" size={60} separator="right">
-        <h3>2024</h3>
+        <h2>2024</h2>
       </TitleWithSeparator>
       <span>R$ 81.000,00</span>
 
       <TitleWithSeparator orientation="vertical" size={60}>
-        <h3>2023</h3>
+        <h2>2023</h2>
       </TitleWithSeparator>
       <span>R$ 81.000,00</span>
 
       <TitleWithSeparator orientation="vertical" size={60}>
-        <h3>2022</h3>
+        <h2>2022</h2>
       </TitleWithSeparator>
       <span>R$ 81.000,00</span>
 
       <TitleWithSeparator orientation="vertical" size={60}>
-        <h3>2021</h3>
+        <h2>2021</h2>
       </TitleWithSeparator>
       <span>R$ 81.000,00</span>
 
       <TitleWithSeparator orientation="vertical" size={60}>
-        <h3>2020</h3>
+        <h2>2020</h2>
       </TitleWithSeparator>
       <span>R$ 81.000,00</span>
 
       <TitleWithSeparator orientation="vertical" size={60}>
-        <h3>2019</h3>
+        <h2>2019</h2>
       </TitleWithSeparator>
       <span>R$ 81.000,00</span>
 
       <TitleWithSeparator orientation="vertical" size={60}>
-        <h3>2018</h3>
+        <h2>2018</h2>
       </TitleWithSeparator>
       <span>R$ 81.000,00</span>
     </TimelineStyle>
