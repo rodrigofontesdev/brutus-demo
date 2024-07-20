e58863c0a212345195de788fdd52001e5a6420cb
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2024-07-19 21:31:36 -0300

    refactor(atom): change background-color on the box component

--- src/components/atoms/Box/styles.ts
+++ src/components/atoms/Box/styles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 export const BoxStyle = styled.div`
   position: relative;
-  background-color: ${(props) => props.theme['blue-700']};
+  background-color: rgb(0 0 0 / 12%);
   border-radius: 0.5rem;
   box-shadow: ${(props) => props.theme.shadow};
 `
