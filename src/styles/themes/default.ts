a7e27cd6add645ed26c4f0b451e88bde275b9e67
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2024-08-01 21:19:55 -0300

    refactor!: rename design tokens

--- src/styles/themes/default.ts
+++ src/styles/themes/default.ts
@@ -1,36 +1,77 @@
-const fontFamily = '"Montserrat", montserrat-fallback'
-
 export const defaultTheme = {
-  'blue-800': '#142133',
-  'blue-700': '#1D314B',
-  'blue-400': '#506BF1',
   'blue-50': '#F6F9FB',
-
-  'red-400': '#F1506B',
+  'blue-400': '#506BF1',
+  'blue-700': '#1D314B',
+  'blue-800': '#142133',
 
   'green-400': '#0EAF94',
 
-  'title-lg': `700 2.25rem ${fontFamily}`,
+  'red-400': '#F1506B',
+
+  'black-alpha-10': 'rgb(0 0 0 / 0.10)',
+  'black-alpha-15': 'rgb(0 0 0 / 0.15)',
+  'black-alpha-20': 'rgb(0 0 0 / 0.20)',
+  'black-alpha-25': 'rgb(0 0 0 / 0.25)',
+  'black-alpha-50': 'rgb(0 0 0 / 0.50)',
+  'black-alpha-75': 'rgb(0 0 0 / 0.75)',
+
+  'font-primary': '"Montserrat", montserrat-fallback',
+
+  'font-xs': '0.75rem',
+  'font-sm': '0.875rem',
+  'font-md': '1rem',
+  'font-lg': '1.125rem',
+  'font-xl': '1.25rem',
+  'font-2xl': '1.625rem',
+  'font-3xl': '2.25rem',
+
+  'font-semibold': 600,
+  'font-bold': 700,
+
+  'line-sm': '1rem',
+  'line-md': '1.25rem',
+  'line-lg': '1.625rem',
+
+  'space-1': '0.3125rem',
+  'space-2': '0.625rem',
+  'space-3': '0.9375rem',
+  'space-4': '1.25rem',
+  'space-5': '1.5625rem',
+  'space-6': '1.875rem',
+  'space-7': '2.1875rem',
+  'space-8': '2.5rem',
+  'space-9': '2.8125rem',
+  'space-10': '3.125rem',
+  'space-20': '6.25rem',
+  'space-30': '9.375rem',
+  'space-40': '12.5rem',
+  'space-50': '15.625rem',
+  'space-60': '18.75rem',
+
+  shadow: '0 4px 4px 0 rgb(0 0 0 / 0.25)',
+  'shadow-inner': 'inset 0 4px 4px 0 rgb(0 0 0 / 0.25)',
+
+  blur: 'blur(5px)',
+
+  'outline-blue': '3px solid rgb(80 107 241 / 0.3)',
+  'outline-green': '3px solid rgb(14 175 148 / 0.3)',
+  'outline-red': '3px solid rgb(241 80 107 / 0.3)',
+
+  'screen-md': '47.5rem',
+  'screen-lg': '85rem',
+
+  /* 'title-lg': `700 2.25rem ${fontFamily}`,
   title: `700 1.625rem ${fontFamily}`,
   'title-sm': `700 1.25rem ${fontFamily}`,
-  'title-xs': `700 1.125rem ${fontFamily}`,
+  'title-xs': `700 1.125rem ${fontFamily}`, */
 
-  text: `600 1rem/1.625rem ${fontFamily}`,
+  /* text: `600 1rem/1.625rem ${fontFamily}`,
   'text-sm': `600 0.875rem/1.25rem ${fontFamily}`,
-  'text-xs': `600 0.75rem/1rem ${fontFamily}`,
+  'text-xs': `600 0.75rem/1rem ${fontFamily}`, */
 
-  'input-xl': `600 2.25rem ${fontFamily}`,
+  /* 'input-xl': `600 2.25rem ${fontFamily}`,
   'input-lg': `600 1.625rem ${fontFamily}`,
   input: `600 1rem ${fontFamily}`,
   'input-sm': `600 0.875rem ${fontFamily}`,
-  'input-xs': `600 0.75rem ${fontFamily}`,
-
-  shadow: '0 4px 4px 0 rgb(0 0 0 / 25%)',
-  'shadow-inner': 'inset 0 4px 4px 0 rgb(0 0 0 / 25%)',
-
-  'backdrop-blur': 'blur(5px)',
-
-  outline: '3px solid rgb(80 107 241 / 30%)',
-  'outline-success': '3px solid rgb(14 175 148 / 30%)',
-  'outline-error': '3px solid rgb(241 80 107 / 30%)'
+  'input-xs': `600 0.75rem ${fontFamily}`, */
 } as const
