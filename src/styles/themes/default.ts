507be663f58546fea85b96ca7bab439c55167ab4
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2024-08-02 21:39:03 -0300

    refactor!: change design tokens structure

--- src/styles/themes/default.ts
+++ src/styles/themes/default.ts
@@ -1,64 +1,118 @@
 export const defaultTheme = {
-  'blue-50': '#F6F9FB',
-  'blue-400': '#506BF1',
-  'blue-700': '#1D314B',
-  'blue-800': '#142133',
+  blue: {
+    50: '#F6F9FB',
+    400: '#506BF1',
+    700: '#1D314B',
+    800: '#142133',
+  },
 
-  'green-400': '#0EAF94',
+  green: {
+    400: '#0EAF94',
+  },
 
-  'red-400': '#F1506B',
+  red: {
+    400: '#F1506B',
+  },
 
-  'black-alpha-10': 'rgb(0 0 0 / 0.10)',
-  'black-alpha-15': 'rgb(0 0 0 / 0.15)',
-  'black-alpha-20': 'rgb(0 0 0 / 0.20)',
-  'black-alpha-25': 'rgb(0 0 0 / 0.25)',
-  'black-alpha-50': 'rgb(0 0 0 / 0.50)',
-  'black-alpha-75': 'rgb(0 0 0 / 0.75)',
+  black: {
+    alpha: {
+      10: 'rgb(0 0 0 / 0.10)',
+      15: 'rgb(0 0 0 / 0.15)',
+      20: 'rgb(0 0 0 / 0.20)',
+      25: 'rgb(0 0 0 / 0.25)',
+      50: 'rgb(0 0 0 / 0.50)',
+      75: 'rgb(0 0 0 / 0.75)',
+    },
+  },
 
-  'font-primary': '"Montserrat", montserrat-fallback',
+  white: {
+    alpha: {
+      20: 'rgb(255 255 255 / 0.20)',
+    },
+  },
 
-  'font-xs': '0.75rem',
-  'font-sm': '0.875rem',
-  'font-md': '1rem',
-  'font-lg': '1.125rem',
-  'font-xl': '1.25rem',
-  'font-2xl': '1.625rem',
-  'font-3xl': '2.25rem',
+  font: {
+    primary: '"Montserrat", montserrat-fallback',
+    semibold: 600,
+    bold: 700,
+    xs: '0.75rem',
+    sm: '0.875rem',
+    md: '1rem',
+    lg: '1.125rem',
+    xl: '1.25rem',
+    '2xl': '1.625rem',
+    '3xl': '2.25rem',
+  },
 
-  'font-semibold': 600,
-  'font-bold': 700,
+  line: {
+    sm: '1',
+    md: '1.25',
+    lg: '1.625',
+  },
 
-  'line-sm': '1rem',
-  'line-md': '1.25rem',
-  'line-lg': '1.625rem',
+  space: {
+    1: '0.3125rem',
+    2: '0.625rem',
+    3: '0.9375rem',
+    4: '1.25rem',
+    5: '1.5625rem',
+    6: '1.875rem',
+    7: '2.1875rem',
+    8: '2.5rem',
+    9: '2.8125rem',
+    10: '3.125rem',
+    20: '6.25rem',
+    30: '9.375rem',
+    40: '12.5rem',
+    50: '15.625rem',
+    60: '18.75rem',
+  },
 
-  'space-1': '0.3125rem',
-  'space-2': '0.625rem',
-  'space-3': '0.9375rem',
-  'space-4': '1.25rem',
-  'space-5': '1.5625rem',
-  'space-6': '1.875rem',
-  'space-7': '2.1875rem',
-  'space-8': '2.5rem',
-  'space-9': '2.8125rem',
-  'space-10': '3.125rem',
-  'space-20': '6.25rem',
-  'space-30': '9.375rem',
-  'space-40': '12.5rem',
-  'space-50': '15.625rem',
-  'space-60': '18.75rem',
+  shadow: {
+    outer: {
+      md: '0 4px 4px 0 rgb(0 0 0 / 0.25)',
+    },
+    inner: {
+      md: 'inset 0 4px 4px 0 rgb(0 0 0 / 0.25)',
+    },
+  },
 
-  shadow: '0 4px 4px 0 rgb(0 0 0 / 0.25)',
-  'shadow-inner': 'inset 0 4px 4px 0 rgb(0 0 0 / 0.25)',
+  blur: {
+    md: 'blur(5px)',
+  },
 
-  blur: 'blur(5px)',
+  outline: {
+    blue: {
+      400: '3px solid #506BF1',
+      alpha: {
+        30: '3px solid rgb(80 107 241 / 0.3)',
+      },
+    },
+    green: {
+      alpha: {
+        30: '3px solid rgb(14 175 148 / 0.3)',
+      },
+    },
+    red: {
+      alpha: {
+        30: '3px solid rgb(241 80 107 / 0.3)',
+      },
+    },
+  },
 
-  'outline-blue': '3px solid rgb(80 107 241 / 0.3)',
-  'outline-green': '3px solid rgb(14 175 148 / 0.3)',
-  'outline-red': '3px solid rgb(241 80 107 / 0.3)',
+  duration: {
+    normal: '400ms',
+  },
 
-  'screen-md': '47.5rem',
-  'screen-lg': '85rem',
+  radii: {
+    md: '0.5rem',
+    full: '100%',
+  },
+
+  screen: {
+    md: '47.5rem',
+    lg: '85rem',
+  },
 
   /* 'title-lg': `700 2.25rem ${fontFamily}`,
   title: `700 1.625rem ${fontFamily}`,
