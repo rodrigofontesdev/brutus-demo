d62edb1dee8b8ada9c9ccd560e00a21d2cacfc1c
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2024-08-02 21:43:23 -0300

    refactor(atom): apply design tokens to divider component styles

--- src/components/atoms/Divider/styles.ts
+++ src/components/atoms/Divider/styles.ts
@@ -2,5 +2,6 @@ import styled from 'styled-components'
 
 export const DividerStyle = styled.div`
   height: 2px;
-  background-color: rgb(80 107 241 / 15%);
+  background-color: ${({ theme }) => theme.blue[400]};
+  opacity: 15%;
 `
