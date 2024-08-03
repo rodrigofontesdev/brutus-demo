8c11375badb047f21f032a477cd7ebdda76c4545
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2024-08-02 21:51:18 -0300

    refactor: apply design tokens on global styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -8,11 +8,27 @@ export const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
   }
 
+  html {
+    scrollbar-width: thin;
+    scrollbar-color:  ${({ theme }) => theme.black.alpha[50]} ${({ theme }) => theme.blue[800]};
+  }
+
   body {
-    background-color: ${(props) => props.theme['blue-700']};
-    color: ${(props) => props.theme['blue-50']};
+    background-color: ${({ theme }) => theme.blue[700]};
+    color: ${({ theme }) => theme.blue[50]};
+    font-family: ${({ theme }) => theme.font.primary};
+    font-weight: ${({ theme }) => theme.font.semibold};
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    font-weight: ${({ theme }) => theme.font.bold};
   }
 
   img {
@@ -26,17 +42,17 @@ export const GlobalStyle = createGlobalStyle`
   }
 
   a {
-    color: ${(props) => props.theme['blue-50']};
+    color: ${({ theme }) => theme.blue[50]};
     text-decoration: none;
-    transition: color 400ms;
+    transition: color ${({ theme }) => theme.duration.normal};
   }
 
   a:hover {
-    color: ${(props) => props.theme['blue-400']};
+    color: ${({ theme }) => theme.blue[400]};
   }
 
   a:focus {
-    outline: 2px solid ${(props) => props.theme['blue-400']}
+    outline: ${({ theme }) => theme.outline.blue}
   }
 
   li {
