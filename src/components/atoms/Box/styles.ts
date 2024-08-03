c51312e55cd30b35ae7f5f503f600f932bf06002
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2024-08-02 21:41:41 -0300

    refactor(atom): apply design tokens to box component styles

--- src/components/atoms/Box/styles.ts
+++ src/components/atoms/Box/styles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 export const BoxStyle = styled.div`
   position: relative;
-  background-color: rgb(0 0 0 / 12%);
-  border-radius: 0.5rem;
-  box-shadow: ${(props) => props.theme.shadow};
+  background-color: ${({ theme }) => theme.black.alpha[15]};
+  border-radius: ${({ theme }) => theme.radii.md};
+  box-shadow: ${({ theme }) => theme.shadow.outer.md};
 `
