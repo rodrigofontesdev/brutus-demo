8aad3e447a0729955cd56c715ace8eda8c52b204
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2024-08-08 20:14:30 -0300

    refactor!: change font sizes tokens

--- src/styles/themes/default.ts
+++ src/styles/themes/default.ts
@@ -41,8 +41,14 @@ export const defaultTheme = {
     md: '1rem',
     lg: '1.125rem',
     xl: '1.25rem',
-    '2xl': '1.625rem',
-    '3xl': '2.25rem',
+    '2xl': '1.5rem',
+    '3xl': '1.75rem',
+    '4xl': '2rem',
+    '5xl': '2.25rem',
+    '6xl': '2.5rem',
+    '7xl': '2.75rem',
+    '8xl': '3rem',
+    '9xl': '3.25rem',
   },
 
   line: {
