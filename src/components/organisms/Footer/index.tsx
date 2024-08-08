daa8e2cb49beda1d302f7d532563ec599f51bd4d
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2024-08-08 20:06:14 -0300

    refactor(organism): remove unnecessary styles on footer component

--- src/components/organisms/Footer/index.tsx
+++ src/components/organisms/Footer/index.tsx
@@ -1,4 +1,4 @@
-import { Links, MainFooter } from './styles'
+import { MainFooter } from './styles'
 
 const currentYear = new Date().getFullYear()
 const appName = import.meta.env.VITE_APP_NAME
@@ -6,22 +6,17 @@ const appName = import.meta.env.VITE_APP_NAME
 export function Footer() {
   return (
     <MainFooter>
-      <Links>
-        <p>
-          &copy; {currentYear} - {appName}. Developed by{' '}
-          <a href="https://rodrigofontes.dev" target="_blank">
-            Rodrigo Fontes
-          </a>
-        </p>
+      <p>
+        &copy; {currentYear} - {appName}.
+      </p>
 
-        <a href="#" target="_blank">
-          Política de privacidade
-        </a>
+      <a href="#" target="_blank">
+        Política de privacidade
+      </a>
 
-        <a href="#" target="_blank">
-          Termos de uso
-        </a>
-      </Links>
+      <a href="#" target="_blank">
+        Termos de uso
+      </a>
     </MainFooter>
   )
 }
--- src/components/organisms/Footer/styles.ts
+++ src/components/organisms/Footer/styles.ts
@@ -2,24 +2,24 @@ import styled from 'styled-components'
 
 export const MainFooter = styled.footer`
   display: flex;
+  flex-direction: column;
   justify-content: center;
-  align-items: center;
+  flex-wrap: wrap;
+  column-gap: ${({ theme }) => theme.space[5]};
+  row-gap: ${({ theme }) => theme.space[2]};
   padding-top: ${({ theme }) => theme.space[3]};
   padding-bottom: ${({ theme }) => theme.space[3]};
   padding-left: ${({ theme }) => theme.space[5]};
   padding-right: ${({ theme }) => theme.space[5]};
-`
-
-export const Links = styled.div`
-  display: flex;
-  justify-content: center;
-  flex-wrap: wrap;
-  gap: ${({ theme }) => theme.space[5]};
-  row-gap: ${({ theme }) => theme.space[2]};
 
   > p,
   a {
     font-size: ${({ theme }) => theme.font.xs};
+    line-height: ${({ theme }) => theme.line.xl};
     text-align: center;
+  }
+
+  @media (min-width: 30rem) {
+    flex-direction: row;
   }
 `
