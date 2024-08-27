dd885c35c88acee1780b9415a4feac3b9a7626f2
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2024-08-26 21:09:21 -0300

    feat(hook): create useFilter() hook

---
+++ src/hooks/useFilter.ts
@@ -0,0 +1,56 @@
+import { useState } from 'react'
+import { MONTHS, YEARS } from '../utils/data'
+
+type ReportingPeriod = {
+  month: number
+  year: number
+}
+
+const currentMonthIndex = new Date().getMonth()
+const currentYear = new Date().getFullYear()
+
+export function useFilter() {
+  const [reportingPeriod, setReportingPeriod] = useState<ReportingPeriod>({
+    month: currentMonthIndex,
+    year: currentYear,
+  })
+
+  const years = getAvailableYears()
+  const months = getAvailableMonths(reportingPeriod.year)
+
+  function onChangeMonth(month: number) {
+    setReportingPeriod({ ...reportingPeriod, month })
+  }
+
+  function onChangeYear(year: number) {
+    setReportingPeriod({ ...reportingPeriod, year })
+  }
+
+  return {
+    months,
+    years,
+    onChangeMonth,
+    onChangeYear,
+  }
+}
+
+function getAvailableMonths(year: number) {
+  const yearOfPeriod = year
+  const isYearOfPeriodEqualsCurrentYear = yearOfPeriod === currentYear
+  const isCurrentMonthDifferentFromJanuary = currentMonthIndex > 0
+
+  if (isYearOfPeriodEqualsCurrentYear && isCurrentMonthDifferentFromJanuary) {
+    const pastMonths = MONTHS.filter((_, index) => index < currentMonthIndex).reverse()
+
+    return pastMonths
+  }
+
+  return [...MONTHS].reverse()
+}
+
+function getAvailableYears() {
+  const isCurrentMonthJanuary = currentMonthIndex === 0
+  const pastYears = YEARS.filter((_, index) => index > 0)
+
+  return isCurrentMonthJanuary ? pastYears : YEARS
+}
