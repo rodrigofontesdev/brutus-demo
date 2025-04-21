33d055de57f527763237f3594976990a71413dbd
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2025-04-21 15:36:28 -0300

    build: use path aliases to import modules

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,18 @@
+import { defaultTheme } from '@static/styles/default-theme'
+import { GlobalStyle } from '@static/styles/global'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { App } from './app'
+import { RouterProvider } from 'react-router-dom'
+import { Toaster } from 'sonner'
+import { ThemeProvider } from 'styled-components'
+import { router } from './router'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <RouterProvider router={router} />
+      <Toaster position="top-right" />
+    </ThemeProvider>
   </React.StrictMode>
 )
