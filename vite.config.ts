320f39a2123c1ce528e26182a86bc4bd30cd8df4
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2025-04-23 21:06:26 -0300

    build: add models and validation as path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -9,11 +9,13 @@ export default defineConfig({
       '@components': path.resolve(__dirname, './src/components'),
       '@contexts': path.resolve(__dirname, './src/contexts'),
       '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@models': path.resolve(__dirname, './src/models'),
       '@pages': path.resolve(__dirname, './src/pages'),
       '@services': path.resolve(__dirname, './src/services'),
       '@static': path.resolve(__dirname, './src/static'),
       '@templates': path.resolve(__dirname, './src/templates'),
       '@utils': path.resolve(__dirname, './src/utils'),
+      '@validation': path.resolve(__dirname, './src/validation'),
     },
   },
 })
