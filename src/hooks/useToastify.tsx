f06f1481e2ecfe94487c92fb6baee6765de57294
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2025-04-23 21:08:58 -0300

    refactor(molecule): change toastify default duration

--- src/hooks/useToastify.tsx
+++ src/hooks/useToastify.tsx
@@ -1,16 +1,13 @@
 import { ExternalToast, toast } from 'sonner'
-import { Toastify, ToastifyTypes } from '../components/molecules/Toastify'
+import { Toastify, ToastifyTypes } from '@components/molecules/Toastify'
 
 type Options = ExternalToast
 
 export function toastify(message: string, type?: ToastifyTypes, options?: Options) {
-  const duration = options?.duration ?? 4000
+  const duration = options?.duration ?? 5000
 
   return toast.custom(
     () => <Toastify message={message} type={type} durationInMilliseconds={duration} />,
-    {
-      ...options,
-      important: true,
-    }
+    { ...options }
   )
 }
