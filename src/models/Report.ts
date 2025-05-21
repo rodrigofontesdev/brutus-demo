25b916236701b1253cb20e253036ad13f263b1dc
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2025-05-20 22:34:55 -0300

    feat(model): create Report model

---
+++ src/models/Report.ts
@@ -0,0 +1,13 @@
+export type Report = {
+  id: string
+  user: string
+  trade_with_invoice: number
+  trade_without_invoice: number
+  industry_with_invoice: number
+  industry_without_invoice: number
+  services_with_invoice: number
+  services_without_invoice: number
+  period: string
+  created_at: string
+  updated_at: string
+}
