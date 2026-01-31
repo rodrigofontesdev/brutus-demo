e5227aa340dd2bfcf748d22ac4ba5fd68570be3c
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2026-01-31 00:08:27 -0300

    feat(service): add method to fetch all report

--- src/services/ReportService.ts
+++ src/services/ReportService.ts
@@ -2,6 +2,29 @@
 import { Report } from '@models/Report'
 import { api } from './axios'
 
+type GetReportsParams = Partial<{
+  year: number
+  order: 'asc' | 'desc'
+  perPage: number
+  cursor: string
+}>
+
+export type GetReportsResponse = {
+  data: Report[]
+  links: {
+    first: string | null
+    last: string | null
+    prev: string | null
+    next: string | null
+  }
+  meta: {
+    path: string
+    per_page: number
+    next_cursor: string | null
+    prev_cursor: string | null
+  }
+}
+
 type CreateReportBody = {
   tradeWithInvoice: number
   tradeWithoutInvoice: number
@@ -15,6 +38,16 @@ type CreateReportBody = {
 export type UpdateReportBody = Omit<CreateReportBody, 'period'>
 
 export class ReportService {
+  static async get(id: string) {
+    const { data } = await api.get<Report>(`/reports/${id}`)
+    return data
+  }
+
+  static async getAll(params: GetReportsParams) {
+    const { data } = await api.get<GetReportsResponse>('/reports', { params })
+    return data
+  }
+
   static async create(body: CreateReportBody) {
     const {
       tradeWithInvoice,
@@ -36,11 +69,6 @@ export class ReportService {
       services_without_invoice: servicesWithoutInvoice,
     })
 
-    return data
-  }
-
-  static async get(id: string) {
-    const { data } = await api.get<Report>(`/reports/${id}`)
     return data
   }
 
